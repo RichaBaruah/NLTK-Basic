7214d9188a9777834c8f3b9684599308321ebe82
RichaBaruah <48044985+RichaBaruah@users.noreply.github.com>
2019-02-28 12:05:16 +0530

    my first commit

---
+++ nltk1_output.py
@@ -0,0 +1,34 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/Admin/Desktop/nltk.py ==================
+Traceback (most recent call last):
+  File "C:/Users/Admin/Desktop/nltk.py", line 1, in <module>
+    import nltk
+  File "C:/Users/Admin/Desktop\nltk.py", line 3, in <module>
+    from nltk.book import *
+ModuleNotFoundError: No module named 'nltk.book'; 'nltk' is not a package
+>>> 
+================== RESTART: C:/Users/Admin/Desktop/nltk1.py ==================
+*** Introductory Examples for the NLTK Book ***
+Loading text1, ..., text9 and sent1, ..., sent9
+Type the name of the text or sentence to view it.
+Type: 'texts()' or 'sents()' to list the materials.
+text1: Moby Dick by Herman Melville 1851
+text2: Sense and Sensibility by Jane Austen 1811
+text3: The Book of Genesis
+text4: Inaugural Address Corpus
+text5: Chat Corpus
+text6: Monty Python and the Holy Grail
+text7: Wall Street Journal
+text8: Personals Corpus
+text9: The Man Who Was Thursday by G . K . Chesterton 1908
+['adventure', 'belles_lettres', 'editorial', 'fiction', 'government', 'hobbies', 'humor', 'learned', 'lore', 'mystery', 'news', 'religion', 'reviews', 'romance', 'science_fiction']
+                  can could   may might  must  will 
+           news    93    86    66    38    50   389 
+       religion    82    59    78    12    54    71 
+        hobbies   268    58   131    22    83   264 
+science_fiction    16    49     4    12     8    16 
+        romance    74   193    11    51    45    43 
+          humor    16    30     8     8     9    13 
+>>> 
